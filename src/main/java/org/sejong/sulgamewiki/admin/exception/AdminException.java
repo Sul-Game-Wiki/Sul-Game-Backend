4180c31f1d93d9b38cbbfc509fd88cd2260ec9ef
username056 <84067473+username056@users.noreply.github.com>
2024-08-08 20:42:54 +0900

    refactor(admin, like): Exception에서 ErrorSource 상수 사용

--- src/main/java/org/sejong/sulgamewiki/admin/exception/AdminException.java
+++ src/main/java/org/sejong/sulgamewiki/admin/exception/AdminException.java
@@ -1,11 +1,13 @@
 package org.sejong.sulgamewiki.admin.exception;
 
+import static org.sejong.sulgamewiki.common.exception.constants.ErrorSource.ADMIN;
+
 import org.sejong.sulgamewiki.common.exception.CustomException;
 import org.sejong.sulgamewiki.common.exception.ErrorCode;
 
 public class AdminException extends CustomException {
 
     public AdminException(ErrorCode errorCode) {
-        super(errorCode, "admin");
+        super(errorCode, ADMIN.name());
     }
 }
--- src/main/java/org/sejong/sulgamewiki/common/like/exception/LikeException.java
+++ src/main/java/org/sejong/sulgamewiki/common/like/exception/LikeException.java
@@ -1,12 +1,14 @@
 package org.sejong.sulgamewiki.common.like.exception;
 
+import static org.sejong.sulgamewiki.common.exception.constants.ErrorSource.LIKE;
+
 import lombok.Getter;
 import org.sejong.sulgamewiki.common.exception.CustomException;
+import org.sejong.sulgamewiki.common.exception.constants.ErrorSource;
 
 @Getter
 public class LikeException extends CustomException {
-
   public LikeException(LikeErrorCode errorCode) {
-    super(errorCode, "like");
+    super(errorCode, LIKE.name());
   }
 }
