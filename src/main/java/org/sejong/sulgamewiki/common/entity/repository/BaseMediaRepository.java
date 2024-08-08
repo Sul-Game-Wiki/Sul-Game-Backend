68c167f5e6e0e61968562ce747a38af51e51d8e4
username056 <84067473+username056@users.noreply.github.com>
2024-08-08 21:06:42 +0900

    refacotr(BMR): 사용 않는 임포트문 삭제

--- src/main/java/org/sejong/sulgamewiki/common/entity/repository/BaseMediaRepository.java
+++ src/main/java/org/sejong/sulgamewiki/common/entity/repository/BaseMediaRepository.java
@@ -1,7 +1,6 @@
 package org.sejong.sulgamewiki.common.entity.repository;
 
 import org.sejong.sulgamewiki.common.entity.BaseMedia;
-import org.sejong.sulgamewiki.common.entity.BasePost;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
